a4d79e82b5d33b713dce101b65012123852b7faa
Olivier <37482393+Olivinsa@users.noreply.github.com>
2019-06-04 16:33:42 +0200

    Update Edge.java

--- GeoSafe/src/gs/Edge.java
+++ GeoSafe/src/gs/Edge.java
@@ -3,18 +3,26 @@ package gs;
 import java.util.ArrayList;
 import java.util.Collections;
 
+//classe désignant les arcs du graphe
 public class Edge {
 	
+	//noeud de départ
 	private String nodeStart;
+	//noeud d'arrivé
 	private String nodeStop;
+	//date (inutilisée)
 	//private int dueDate;
+	//longueur de l'arc
 	private int length;
+	//capacité de l'arc
 	private int capacity;
+	//listes des taches appliquées sur l'arc
 	private ArrayList<Task> tabTasks = new ArrayList<Task>();
 	
+	//constructeur de l'arc à partir d'une ligne 
+	//utilisé pour la construction d'un graphe à partir des fichiers de données
 	public Edge(String ligne) {
 		String[] liste = ligne.split(" ");
-
 		this.nodeStart = liste[0];
 		this.nodeStop = liste[1];
 		//this.dueDate = Integer.parseInt(liste[2]);
@@ -22,10 +30,12 @@ public class Edge {
 		this.capacity = (int) Math.round(Integer.parseInt(liste[4]));
 	}
 	
+	//fonction pour savoir si l'arc est celui désigné par les 2 id de noeuds donnés
 	public boolean isEdge(String nodeA, String nodeB) {
 		return nodeStart.equals(nodeA) && nodeStop.equals(nodeB);
 	}
 	
+	//ajoute une tache dans l'arc
 	public void addTask(int id,  int population, double decalage, int taux) {
 		Task t = new Task(id, population, decalage, taux);
 		//supprime la tache du meme id si elle existe
@@ -34,7 +44,7 @@ public class Edge {
 		tabTasks.add(t);
 	}
 	
-	
+	//supprime une tache de l'arc
 	public void delTask(int id) {
 		for(int i =0; i<tabTasks.size(); i++) {
 			Task t = tabTasks.get(i);
@@ -52,6 +62,7 @@ public class Edge {
 		return capacity;
 	}
 	
+	//recupere le décalage de la tache donnée (par son id)
 	public double getDecalage(int id) {
 		for(Task t : tabTasks) {
 			if(t.getId()==id) {
@@ -66,6 +77,7 @@ public class Edge {
 		return tabTasks;
 	}
 	
+	//recupere la tache donnée (par son id)
 	public Task getTask(int id) {
 		for(Task t : tabTasks) {
 			if(t.getId()==id) {
@@ -76,6 +88,7 @@ public class Edge {
 		return new Task(-1,0,0,0);
 	}
 	
+	//indique si le noeud est l'une des extremités de l'arc
 	public boolean isEnd(String node) {
 		return node.equals(nodeStop) || node.equals(nodeStart);
 	}
@@ -85,6 +98,7 @@ public class Edge {
 	}
 	
 	//verifie si la capacité de l'arc n'est pas dépassée par les taches qui l'utilise
+	// -> true si la capacité n'est pas dépassée ; false sinon
 	public boolean hasEnoughCapacityForTasks() {
 		int MaxUsedCapacity = 0;
 		ArrayList<Double> listeTimes = new ArrayList<Double>();
@@ -93,16 +107,18 @@ public class Edge {
 			listeTimes.add(t.getDecalage());
 			listeTimes.add(t.getDecalage()+t.getDuree());
 		}
-		//pour chaque temps, on somme les taux utilisés par la taches à ces temps
+		//pour chaque temps, on somme les taux utilisés par les taches à ces temps
 		for(double time: listeTimes) {
 			int capTime = calcCurrentTaux(time);
 			//capacité maximale utilisé pour l'arc sur toute la durée de l'evacuation
 			MaxUsedCapacity = Math.max(MaxUsedCapacity, capTime);
 		}
 		if(MaxUsedCapacity <= capacity) {
+			//la capacité n'est pas dépassée
 			return true;
 		}
 		else {
+			//la capacité est dépassée
 			//System.out.println(toString()+" capacity ="+capacity+" MaxUsedCapacity = "+MaxUsedCapacity );
 			for(Task t: getListeTasks()) {
 				//System.out.println("    Tache "+t.getId()+" debut "+t.getDecalage()+ "  fin "+(t.getDecalage()+t.getDuree())+"  taux "+t.getTaux());
@@ -119,26 +135,35 @@ public class Edge {
 			listeTimes.add(t1.getDecalage());
 			listeTimes.add(t1.getDecalage()+t1.getDuree());
 		}
+		//on trie dans l'ordre des temps croissants cette liste
 		Collections.sort(listeTimes);
 		//pour chaque temps, on regarde si on peut placer la tache
 		for(double time: listeTimes) {
 			if(time >= t.getDecalage()) {
+				//on ajoute la tache pour tester
 				addTask(t.getId(), t.getPopulation(), time, t.getTaux());
 				if(hasEnoughCapacityForTasks()) {
+					//si la capacité n'est pas dépassée, on retire la tache et on donne le temps
 					delTask(t.getId());
 					return time;
 				}
 				else {
+					//sinon on retire la tache et on teste avec le temps suivant
 					delTask(t.getId());
 				}
 			}
 		}
+		//techniquement c'est impossible de ne pas trouver de temps disponible, 
+		//car si on place la tache au dernier temps possible la capacité ne sera pas dépassée
+		//ou alors la tache est trop "large" pour cet arc
 		System.out.println("pas de moment dispo trouvé");
 		System.exit(0);
 		return -1;
 	}
 	
 	//renvoie le taux pour ajouter une tache t dans l'arc
+	//la tache est ajoutée à un temps fixe, mais du coup on essaye de faire varier son taux
+	//pour qu'elle rentre dans l'arc
 	public int getAvailableTaux(Task t) {
 		
 		double startTime = t.getDecalage();
@@ -146,32 +171,42 @@ public class Edge {
 		int maxTaux = Math.min(t.getTaux(), capacity-calcCurrentTaux(startTime));
 		
 		ArrayList<Double> listeTimes = new ArrayList<Double>();
-		//on fait un tab qui contient tous les temps de début et fin de taches
+		//on fait une liste qui contient tous les temps de début et fin de taches
 		for(Task t1: getListeTasks()) {
 			listeTimes.add(t1.getDecalage());
 			listeTimes.add(t1.getDecalage()+t1.getDuree());
 		}
+		//on trie cette liste
 		Collections.sort(listeTimes);
-		//pour chaque temps, on regarde si on peut placer la tache
+		//pour chaque temps, on regarde le taux necessaire pour que la tache se termine à ce temps
 		for(double time: listeTimes) {
 			if(time > startTime) {
-				
+				//on réduit le taux, en faisant le min de l'ancien et du taux disponible à ce temps là
 				maxTaux = Math.min(maxTaux, capacity-calcCurrentTaux(time));
 				if(maxTaux == 0) {
+					//si maxtaux = 0, c'est qu'une autre tache va prendre toute la place apres celle qu'on veut placer,
+					//et qu'il est alors impossible de placer la tache quel que soit le taux
 					return 0;
 				}
+				//si la tache peut faire passer toute sa population avec ce taux avant le temps qu'on est en train de regarder
+				//alors ce taux est convenable et on le renvoie
 				if( (time-startTime)*maxTaux >= t.getPopulation()) {
 					return maxTaux;
 				}
 			}
 		}
+		//on a vu tous les temps et la tache n'est jamais passé,
+		//le taux renvoyé est donc le plus petit taux disponible dans l'arc apres le début de la tache
+		//la tache peut utiliser ce taux pour que la capacité de l'arc ne soit jamais dépassée, 
+		//elle ne peut pas utiliser un taux plus grand, et si elle utilise un taux plus petit elle va juste durer plus longtemps,
+		//donc c'est bien le meilleur taux qu'elle peut utiliser
 		return maxTaux;
 					
 		
 		
 	}
 	
-	//calcule le taux utilisé à ce temps là
+	//calcule la capacité utilisé par les taches de l'arc au moment donné
 	public int calcCurrentTaux(double time) {
 		int currenCap = 0;
 		for(Task t: getListeTasks()) {
