5a697a6fe3bd783b05393faaaae3f27d33710443
Olivier <37482393+Olivinsa@users.noreply.github.com>
2019-06-04 17:17:06 +0200

    Update Node.java

--- GeoSafe/src/gs/Node.java
+++ GeoSafe/src/gs/Node.java
@@ -1,11 +1,17 @@
 package gs;
 
+//classe pour representer un noeud
+//usuellement, on n'en a pas bsoin et on ne se sert que des id
+//on ne l'utilise que pour les noeuds de départs de chemins d'évacuation
 public class Node {
 	
 	private String id;
+	//taux d'évacuation du chemin partant de ce noeud
 	private int evacRate;
+	//temps de départ de l'évacuation
 	private double evacStart;
 	
+	//construction du noeud à partir d'une ligne d'un fichier solution
 	public Node(String ligne) {
 		String[] liste = ligne.split(" ");
 		this.id = liste[0];
@@ -13,6 +19,7 @@ public class Node {
 		this.evacStart = Double.parseDouble(liste[2]);
 	}
 	
+	//construction du noeud avec les parametres
 	public Node(String id, int evacRate, double evacStart) {
 		this.id = id;
 		this.evacRate = evacRate;
