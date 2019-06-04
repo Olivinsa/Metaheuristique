0bc083036970163f8f9d10743943915501aad655
Olivier <37482393+Olivinsa@users.noreply.github.com>
2019-06-04 17:37:29 +0200

    Update Task.java

--- GeoSafe/src/gs/Task.java
+++ GeoSafe/src/gs/Task.java
@@ -1,8 +1,11 @@
 package gs;
 
+//classe représentant une tache
+//l'accent de tache est omis volontairement
 public class Task {
 	
 	private int id;
+	//nombre de personne à évacuer avec cette tache
 	private int population;
 	private double decalage;
 	private int taux;
@@ -14,6 +17,9 @@ public class Task {
 		taux = t;
 	}
 
+	//la durée d'une tache est définie par sa population divisée par son taux
+	//on a décidé que la durée serait un double pour éviter des arrondis qui fausseraient le reste
+	//le site de tests en ligne a des durées en int, du coup nos solutions ne sont pas compatibles avec 
 	public double getDuree() {
 		//return Math.round((double)population/(double)taux);
 		return (double)population/(double)taux;
